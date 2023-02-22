4633d9e0b3631d1f0125a1f976dd500d1e917a5f
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-02-22 20:55:08 +1000

    29

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите длину массива");
+int size = int.Parse(Console.ReadLine());
+int[] Array(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(1, 99);
+    }
+    return result;
+}
+int[] array = Array(size);
+Console.WriteLine(String.Join("-", array));
